92001adb20afd9b17f128d65227b0d71f0035063
DarkEden-Coding <darkedenc9@gmail.com>
2026-05-14 13:11:01 -0400

    Prevent icon from expanding apon open

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,5 +1,5 @@
 import { app, BrowserWindow, dialog } from "electron";
-import { createMainWindow, getAppIconPath } from "./app/createWindow";
+import { createMainWindow } from "./app/createWindow";
 import { createApplicationMenu } from "./app/menu";
 import { registerIpc } from "./ipc/registerIpc";
 import { ensureTerminalPastePermissions } from "./services/permissionsService";
@@ -46,8 +46,6 @@ function openMainWindow(): void {
 }
 
 app.whenReady().then(() => {
-  if (process.platform === "darwin") app.dock?.setIcon(getAppIconPath());
-
   registerIpc({
     getMainWindow: () => mainWindow,
     getTerminalManager: () => ptyManager
